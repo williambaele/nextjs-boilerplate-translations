31d43847c4f56a6030d86c654572839c73503ec6
William Baele <baelewilliam1999@gmail.com>
2024-05-30 14:04:20 +0200

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   rules: {
     quotes: "off",
     singleQuote: "off",
-    "prettier/prettier": ["error", { endOfLine: "Auto" }],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
   overrides: [
     {
