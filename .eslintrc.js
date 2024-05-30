257ed292130341b8c9f490b643d3b6b2405ea9ce
William Baele <baelewilliam1999@gmail.com>
2024-05-30 14:27:23 +0200

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   env: {
     node: true,
+    es6: true,
   },
   extends: [
     "molindo/typescript",
     "molindo/react",
     "molindo/tailwind",
     "plugin:@next/next/recommended",
+    "plugin:prettier/recommended" // Ensure this is added to extend Prettier's configuration
   ],
   rules: {
     quotes: "off",
     singleQuote: "off",
     "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "spaced-comment": ["error", "always", { "exceptions": ["///"] }] // Fix for spaced-comment error
   },
   overrides: [
     {
@@ -22,4 +25,5 @@ module.exports = {
       },
     },
   ],
+  ignorePatterns: [".eslintrc.js", "*"]
 };
